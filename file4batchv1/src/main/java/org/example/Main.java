bc83126b236afc536fb199784e977284a5b7eba8
u4git <wang-city@163.com>
2026-04-23 08:36:10 +0000

    Modified wordcount4 to file4

---
+++ file4batchv1/src/main/java/org/example/Main.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("This is flink2demo file4batchv1...");
+
+        // 创建环境
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setParallelism(1);
+
+        env.setRuntimeMode(RuntimeExecutionMode.BATCH);
+
+        // 读取数据
+
+        FileSource<String> fileSource = FileSource.forRecordStreamFormat(new TextLineInputFormat(), new Path("words.txt")).build();
+
+        DataStreamSource<String> lines = env.fromSource(fileSource, WatermarkStrategy.noWatermarks(), "LocalTextFile");
+
+        // 分词
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordPairs = lines.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] words = value.split(" ");
+
+                for (String word : words) {
+                    Tuple2<String, Integer> t = Tuple2.of(word, 1);
+
+                    out.collect(t);
+                }
+            }
+        });
+
+        // 分组
+
+        KeyedStream<Tuple2<String, Integer>, String> wordGroups = wordPairs.keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> value) throws Exception {
+                return value.f0;
+            }
+        });
+
+        // 统计
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordCount  = wordGroups.sum(1);
+
+        // 打印结果
+        wordCount.print();
+
+        // 执行
+        env.execute("file4batchv1");
+
+        System.out.println("This is flink2demo file4batchv1...done.");
+    }
+}
---
+++ file4streamv2/src/main/java/org/example/Main.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.connector.dsv2.DataStreamV2SinkUtils;
+import org.apache.flink.api.connector.dsv2.DataStreamV2SourceUtils;
+import org.apache.flink.api.connector.sink2.Sink;
+import org.apache.flink.api.connector.sink2.SinkWriter;
+import org.apache.flink.api.connector.sink2.WriterInitContext;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.datastream.api.ExecutionEnvironment;
+import org.apache.flink.datastream.api.common.Collector;
+import org.apache.flink.datastream.api.context.NonPartitionedContext;
+import org.apache.flink.datastream.api.context.PartitionedContext;
+import org.apache.flink.datastream.api.function.OneInputStreamProcessFunction;
+import org.apache.flink.datastream.api.stream.NonKeyedPartitionStream;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 运行时需要添加以下 JVM 参数解决模块访问问题：
+ * --add-opens java.base/java.util=ALL-UNNAMED
+ * --add-opens java.base/java.lang=ALL-UNNAMED
+ * --add-opens java.base/java.lang.reflect=ALL-UNNAMED
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("This is flink2demo file4streamv2...");
+
+        // 创建环境
+
+        ExecutionEnvironment env = ExecutionEnvironment.getInstance();
+
+        env.setExecutionMode(RuntimeExecutionMode.STREAMING);
+
+        // 读取数据
+
+        FileSource<String> fileSource = FileSource.forRecordStreamFormat(new TextLineInputFormat(), new Path("words.txt")).build();
+
+        NonKeyedPartitionStream.ProcessConfigurableAndNonKeyedPartitionStream<String> lines = env.fromSource(DataStreamV2SourceUtils.wrapSource(fileSource), "LocalTextFile");
+
+        // 分词
+
+        NonKeyedPartitionStream.ProcessConfigurableAndNonKeyedPartitionStream<Tuple2<String, Integer>> wordPairs = lines.process(new OneInputStreamProcessFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void processRecord(String record, Collector<Tuple2<String, Integer>> output, PartitionedContext<Tuple2<String, Integer>> ctx) throws Exception {
+                String[] words = record.split(" ");
+
+                for (String word : words) {
+                    Tuple2<String, Integer> t = Tuple2.of(word, 1);
+                    output.collect(t);
+                }
+            }
+        });
+
+        // 统计
+        NonKeyedPartitionStream.ProcessConfigurableAndNonKeyedPartitionStream<Tuple2<String, Integer>> wordCount = wordPairs.process(new OneInputStreamProcessFunction<Tuple2<String, Integer>, Tuple2<String, Integer>>() {
+
+            private Map<String, Integer> wordCounts;
+
+            @Override
+            public void open(NonPartitionedContext<Tuple2<String, Integer>> ctx) throws Exception {
+                OneInputStreamProcessFunction.super.open(ctx);
+                this.wordCounts = new HashMap<>();
+            }
+
+            @Override
+            public void processRecord(Tuple2<String, Integer> record, Collector<Tuple2<String, Integer>> output, PartitionedContext<Tuple2<String, Integer>> ctx) throws Exception {
+                String word = record.f0;
+                int count = wordCounts.getOrDefault(word, 0) + record.f1;
+                this.wordCounts.put(word, count);
+                output.collect(Tuple2.of(word, count));
+            }
+
+            @Override
+            public void endInput(NonPartitionedContext<Tuple2<String, Integer>> ctx) throws Exception {
+                // Nothing.
+            }
+        });
+
+        // 打印结果
+        wordCount.toSink(DataStreamV2SinkUtils.wrapSink(new Sink<Tuple2<String, Integer>>() {
+            @Override
+            public SinkWriter<Tuple2<String, Integer>> createWriter(WriterInitContext context) throws IOException {
+                return new SinkWriter<Tuple2<String, Integer>>() {
+                    @Override
+                    public void write(Tuple2<String, Integer> element, Context context) {
+                        System.out.println(element);
+                    }
+
+                    @Override
+                    public void flush(boolean endOfInput) {
+                    }
+
+                    @Override
+                    public void close() {
+                    }
+                };
+            }
+        }));
+
+        // 执行
+        env.execute("file4streamv2");
+
+        System.out.println("This is flink2demo file4streamv2...done.");
+    }
+}
